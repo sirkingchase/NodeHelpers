3c6ed98e6dde031e662f491ef9a88b361b427d84
Chase <sirkingchase@gmail.com>
2022-10-28 09:25:29 -0500

    Update faker.js

--- faker.js
+++ faker.js
@@ -1,3 +1,4 @@
+
 const faker = require('faker');
 
 var fakeObj = {};
@@ -12,4 +13,22 @@ myData.forEach(function(fu) {
         fakeObj[objs[i].valueOf()] = faker[fu.valueOf()][objs[i].valueOf()]();
     }
 });
+
+var randomNum = parseInt((Math.abs(Math.random() * 100))) + 31;
+
+fakeObj.username = fakeObj.firstName + '.' + fakeObj.lastName + '_' + randomNum;
+
+fakeObj.email = fakeObj.username + '@helloworld.com';
+
+var arrStr = ['Male', 'Female'];
+fakeObj.gender = arrStr[Math.floor(Math.random() * arrStr.length)];
+
+fakeObj.birthDate = new Date(faker.date.between('1960-01-01T00:00:00.000Z', '2000-01-01T00:00:00.000Z')).toLocaleDateString('en-US');
+
+fakeObj.soon = new Date(new Date().setDate(new Date().getDate() + randomNum )).toLocaleDateString('en-US');
+
+fakeObj.zipCodeByState = fakeObj.zipCodeByState.substring(0, 5)
+
 console.log(JSON.stringify(fakeObj));
+
+exportsTest.fake = fakeObj;
